eb32b37973e752a06580757fed2eb5bba76dfc59
Akila W <akila.wickramasekara@ucdconnect.ie>
2024-08-12 20:59:28 +0100

    bugfix

--- API/Automation/summery.py
+++ API/Automation/summery.py
@@ -90,7 +90,7 @@ def process_test_results(cursor, job_id, base_test_case):
             WHERE job_id = %s AND base_test_case like %s
         """
         query_code_exec_count = """SELECT count(*) as code_execution_count FROM `test_results` 
-                                WHERE job_id = %s AND base_test_case like %s AND results != '';"""
+                                WHERE job_id = %s AND base_test_case like %s AND error == '';"""
         cursor.execute(query_code_exec_count, (job_id, f'%{base_test_case}'))
         code_exec_count = cursor.fetchone()
         code_exec_count = code_exec_count[0]
@@ -151,6 +151,7 @@ def process_test_results(cursor, job_id, base_test_case):
                 summary_dict[(job_id, base_test_case)]['model'] = model
         summary_dict[(job_id, base_test_case)]['code_execution_count'] = code_exec_count
         summary_dict[(job_id, base_test_case)]['errors_count'] = code_error_count
+        summary_dict[(job_id, base_test_case)]['total_code_executions'] = len(rows)
         print(summary_dict)
         return summary_dict, model
 
