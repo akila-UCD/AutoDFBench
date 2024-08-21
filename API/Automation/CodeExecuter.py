031f74e0fa6223e5f876e054fc83b7a9b4f5cc5c
Akila W <akila.wickramasekara@ucdconnect.ie>
2024-08-21 21:25:28 +0100

    add base test case attribute

--- API/Automation/CodeExecuter.py
+++ API/Automation/CodeExecuter.py
@@ -6,6 +6,7 @@ import sys
 from mysql.connector import Error
 
 job_id = sys.argv[1]
+base_test_case = sys.argv[2]
 
 # Function to check and copy necessary disk image files
 def check_and_copy_disk_images():
@@ -75,7 +76,11 @@ def process_scripts(conn, base_folder, output_folder):
     try:
         conn.reconnect()
         cursor = conn.cursor(dictionary=True)
-        query = "SELECT file_path, script_type, model FROM prompt_codes WHERE job_id = %s"
+        if(base_test_case is None):
+            query = "SELECT file_path, script_type, model FROM prompt_codes WHERE job_id = %s"
+        else:
+            query = f"SELECT file_path, script_type, model FROM prompt_codes WHERE job_id = %s and base_test_case = {base_test_case}"
+        
         cursor.execute(query, (job_id,))
         rows = cursor.fetchall()
 
