7b2d8775b3b80ed5719e8e241b1e198742dd7f51
Akila W <akila.wickramasekara@ucdconnect.ie>
2024-09-27 18:08:19 +0100

    execution limit defined

--- API/Automation/summery.py
+++ API/Automation/summery.py
@@ -180,7 +180,7 @@ def process_test_results(cursor, job_id, base_test_case):
         """
         query_code_exec_count = """SELECT count(*) as code_execution_count FROM `test_results` 
                                 WHERE job_id = %s AND base_test_case like %s AND error = '';"""
-        cursor.execute(query_code_exec_count, (job_id, f'%{base_test_case}'))
+        cursor.execute(query_code_exec_count, (job_id, f'%{base_test_case}'), take_in_count)
         code_exec_count = cursor.fetchone()[0]
 
         query_error_count = """SELECT count(*) as error_count FROM `test_results` 
@@ -188,7 +188,7 @@ def process_test_results(cursor, job_id, base_test_case):
         cursor.execute(query_error_count, (job_id, f'%{base_test_case}', take_in_count))
         code_error_count = cursor.fetchone()[0]
 
-        cursor.execute(query, (job_id, f'%{base_test_case}'), take_in_count)
+        cursor.execute(query, (job_id, f'%{base_test_case}'))
         rows = cursor.fetchall()
 
         summary_dict = {}
