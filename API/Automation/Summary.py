d374fa0cff10faa9fe2561620f82a77b6a535357
Akila W <akila.wickramasekara@ucdconnect.ie>
2024-10-11 16:46:05 +0100

    removedebug

--- API/Automation/Summary.py
+++ API/Automation/Summary.py
@@ -394,8 +394,8 @@ def calScoreCal(job_id):
     else:
         ops = 'linux'
 
-    # q_fetch_results = f"SELECT id,base_test_case,results FROM `test_results` WHERE `job_id` = {job_id} and score_cal_status = 0;"
-    q_fetch_results = f"SELECT id,base_test_case,results FROM `test_results` WHERE `id` = 19979"
+    q_fetch_results = f"SELECT id,base_test_case,results FROM `test_results` WHERE `job_id` = {job_id} and score_cal_status = 0;"
+    # q_fetch_results = f"SELECT id,base_test_case,results FROM `test_results` WHERE `id` = 19979"
     cursor.execute(q_fetch_results)
     rows = cursor.fetchall()
     result_ary = {}
@@ -463,8 +463,7 @@ def calScoreCal(job_id):
 def count_true_false_positives_negatives(text, array, fn_ary,status):
 
     lines = text.splitlines()
-    print(text)
-    print(array)
+
     # First regex pattern to check for status in angle brackets
     pattern_brackets = r"(\d{4})[^0-9]*?<([a-zA-Z]+)>"
     # Second regex pattern to capture the last word if no match is found
@@ -510,7 +509,7 @@ def count_true_false_positives_negatives(text, array, fn_ary,status):
             if (status.lower(), str_id) not in matched_set:
                 false_negative_count += 1
     print("true_positive_count:", true_positive_count) 
-    os._exit(1)
+
     return true_positive_count, false_positive_count, false_negative_count
         
 
